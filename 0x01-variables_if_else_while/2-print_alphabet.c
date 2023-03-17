e24b9416b5133bfb753c791413288d515cea2a9d
sakachris <sakachris90@gmail.com>
2023-03-17 07:53:25 -0700

    alphabets lower case

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/* printing alphabets in lower case */
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 if success
+ */
+int main(void)
+{
+	char c;
+
+	/* putchar alphabets from a to z */
+	for (c = 'a'; c <= 'z'; c++)
+	putchar(c);
+	return (0);
+}
