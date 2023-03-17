336e7e13f680e24b1958a9a74b79123101334c15
sakachris <sakachris90@gmail.com>
2023-03-17 11:44:12 -0700

    comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/* printing numbers 0 to 9 */
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 if success
+ */
+int main(void)
+{
+	int i;
+
+	/* putchar numbers 0 to 9 */
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i % 10 + '0');
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
