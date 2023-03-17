7438fecc7b2df958fa6e454d0c9cabb12ed99536
sakachris <sakachris90@gmail.com>
2023-03-17 10:15:02 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/* printing numbers 0 to 9 */
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 if success
+ */
+int main(void)
+{
+	int i;
+
+	/* printf numbers 0 to 9 */
+	for (i = 0; i < 10; i++)
+		printf("%i", i);
+	return (0);
+}
