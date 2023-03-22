be746b7440ce510e660e686e3aeb879209e00e5c
sakachris <sakachris90@gmail.com>
2023-03-22 13:22:29 -0700

    fib

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,15 +7,15 @@
 
 int main(void)
 {
+	unsigned int n1 = 1, n2 = 2;
+	unsigned int nn = n1 + n2;
 	int i;
-	long unsigned int n1 = 1, n2 = 2;
-	long unsigned int nn = n1 + n2;
 
-	printf("%lu, %lu, ", n1, n2);
+	printf("%u, %u, ", n1, n2);
 
 	for (i = 5; i <= 52; i++)
 	{
-		printf("%lu, ", nn);
+		printf("%u, ", nn);
 		n1 = n2;
 		n2 = nn;
 		nn = n1 + n2;
