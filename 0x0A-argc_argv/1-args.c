d73b2eda7277408f50d54c65b5db870e87bef438
sakachris <sakachris90@gmail.com>
2023-04-09 00:13:52 -0700

    number of arguments passed

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: argument counter
+ * @argv: pointer to arguments passed
+ *
+ * Return: 0 if success
+ */
+
+int main(int argc, char *argv[])
+{
+
+	(void)argv;
+
+	printf("%d\n", argc - 1);
+	return (0);
+}
