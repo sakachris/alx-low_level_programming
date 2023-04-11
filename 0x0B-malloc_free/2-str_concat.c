24145ba578a0dbc8bc239f5dbc81179e5ef541f2
sakachris <sakachris90@gmail.com>
2023-04-11 13:06:15 -0700

    concatenate

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -22,11 +22,11 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 == NULL)
 	{
-		s1 = " ";
+		s1 = "";
 	}
 	if (s2 == NULL)
 	{
-		s2 = " ";
+		s2 = "";
 	}
 
 	cat = malloc(sizeof(char) * new_size);
