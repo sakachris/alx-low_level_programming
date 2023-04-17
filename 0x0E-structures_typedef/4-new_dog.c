9ccd04a3aac1030bbc6294947ce2799c6e27a0b0
sakachris <sakachris90@gmail.com>
2023-04-17 15:32:16 -0700

    free dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -17,6 +17,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog = malloc(sizeof(dog_t));
 	if (!dog)
 	{
+		free(dog);
 		return (NULL);
 	}
 
--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -10,5 +10,8 @@
 
 void free_dog(dog_t *d)
 {
-	free(d);
+	if (d != NULL)
+	{
+		free(d);
+	}
 }
