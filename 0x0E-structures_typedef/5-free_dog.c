d5a78eea036725082ad5a27377b7660cd479a0cf
sakachris <sakachris90@gmail.com>
2023-04-17 13:42:21 -0700

    free dog

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - frees malloc memory of a struct
+ * @d: struct to be freed
+ *
+ * Return: Nothing
+ */
+
+void free_dog(dog_t *d)
+{
+	free(d);
+}
