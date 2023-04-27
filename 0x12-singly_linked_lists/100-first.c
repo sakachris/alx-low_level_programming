b1b79040c693745b5e55b0305923cb9dcf4e27f4
sakachris <sakachris90@gmail.com>
2023-04-27 10:04:01 -0700

    task 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+/**
+ * print_before_main - executes to print before main function
+ *
+ * Return: nothing
+ */
+
+void print_before_main(void)
+{
+	printf("You're beat! and yet, you must allow,");
+	printf("\nI bore my house upon my back!\n");
+}
