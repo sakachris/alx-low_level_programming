37f7ba70c605cab02e65e5bd7e25d9ef2b9837b8
sakachris <sakachris90@gmail.com>
2023-05-09 07:28:05 -0700

    copy6

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,6 +1,22 @@
 #include "main.h"
 
 /**
+ * closing - closes open files
+ * @fd: file descriptor of the open file
+ *
+ * Return: nothing
+ */
+
+void closing(int fd)
+{
+	if (close(fd) < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
+
+/**
  * main - entry point
  * @ac: count of arguments
  * @av: arguments passed
@@ -28,21 +44,18 @@ int main(int ac, char **av)
 	fd_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC | O_APPEND, 00664);
 	while ((reading = read(fd_from, space, 1024)) > 0)
 		writing = write(fd_to, space, reading);
-	if (writing < 0 || reading < 0 || fd_to < 0)
+	if (writing < 0 || fd_to < 0)
 	{
 		dprintf(STDERR_FILENO, "Can't write to %s\n", av[2]);
 		exit(99);
 	}
-	if (close(fd_from) < 0)
+	if (reading < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
-		exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
 	}
-	if (close(fd_to) < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
-		exit(100);
-	}
+	closing(fd_from);
+	closing(fd_to);
 	free(space);
 	return (0);
 }
