e608b22330a6647dce172a043600227c7e3739c1
sakachris <sakachris90@gmail.com>
2023-05-12 00:36:55 -0700

    copy7

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -44,7 +44,7 @@ int main(int ac, char **av)
 	fd_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC | O_APPEND, 00664);
 	while ((reading = read(fd_from, space, 1024)) > 0)
 		writing = write(fd_to, space, reading);
-	if (writing < 0 || fd_to < 0)
+	if ((writing != reading) || fd_to < 0)
 	{
 		dprintf(STDERR_FILENO, "Can't write to %s\n", av[2]);
 		exit(99);
