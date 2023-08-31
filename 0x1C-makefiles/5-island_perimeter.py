d590eb5097b5be2027fc1a6913beaa5a8da11a71
sakachris <sakachris90@gmail.com>
2023-08-31 12:51:42 +0300

    fixing perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -12,23 +12,35 @@ def island_perimeter(grid):
     columns = len(grid[0])
     perimeter = 0
     ones = 0
+    zeros = 0
 
     for i in range(rows):
         for j in range(columns):
             if grid[i][j] == 1:
                 if j > 0 and grid[i][j-1] == 1:
                     ones += 1
+                elif j > 0 and grid[i][j-1] == 0:
+                    zeros += 1
                 if j < columns-1 and grid[i][j+1] == 1:
                     ones += 1
+                elif j < columns-1 and grid[i][j+1] == 0:
+                    zeros += 1
                 if i > 0 and grid[i-1][j] == 1:
                     ones += 1
+                elif i > 0 and grid[i-1][j] == 0:
+                    zeros += 1
                 if i < rows-1 and grid[i+1][j] == 1:
                     ones += 1
+                elif i < rows-1 and grid[i+1][j] == 0:
+                    zeros += 1
             if ones == 3:
                 perimeter += 1
             elif ones == 2:
                 perimeter += 2
             elif ones == 1:
                 perimeter += 3
+            elif zeros == 4:
+                perimeter = 4
             ones = 0
+            zeros = 0
     return perimeter
