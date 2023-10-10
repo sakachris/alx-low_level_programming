e826ac4183e0fb3f388447e398c8f58809b437ff
sakachris <sakachris90@gmail.com>
2023-10-10 19:45:53 +0300

    jump algorithm

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,42 @@
+#include "search_algos.h"
+
+/**
+ * jump_search - searches for a value using jump search algorithm
+ * @array: array to search
+ * @size: size of the array
+ * @value: integer to be searched
+ *
+ * Return: first index where value is located or -1 otherwise
+ */
+
+int jump_search(int *array, size_t size, int value)
+{
+	int s = size, step, next, prev = 0;
+
+	if (!array)
+		return (-1);
+
+	step = sqrt(s);
+
+	while (prev < s)
+	{
+		if (array[prev] >= value)
+			break;
+		printf("Value checked array [%d] = [%d]\n", prev, array[prev]);
+		prev += step;
+	}
+
+	next = prev - step;
+	printf("Value found between indexesn[%d] and [%d]\n", next, prev);
+	if (prev >= s)
+		prev = s - 1;
+
+	while (next <= prev)
+	{
+		printf("Value checked array [%d] = [%d]\n", next, array[next]);
+		if (array[next] == value)
+			return (next);
+		next++;
+	}
+	return (-1);
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -2,6 +2,10 @@
 #define __SEARCH_ALGOS_H__
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
 #endif /* __SEACH_ALGOS_H__ */
